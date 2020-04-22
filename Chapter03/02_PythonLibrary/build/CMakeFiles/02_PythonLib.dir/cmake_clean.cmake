7c5d149eae66adfd0fd09762118e918d648a4333
Visin1991 <zhuzhanhao1991@gmail.com>
2020-04-23 01:09:52 +0800

    add CMake Python Example

---
+++ Chapter03/02_PythonLibrary/build/CMakeFiles/02_PythonLib.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/02_PythonLib.dir/hello-python.c.o"
+  "02_PythonLib.pdb"
+  "02_PythonLib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/02_PythonLib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
