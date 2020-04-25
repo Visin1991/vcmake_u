7c437ac136d8bfea462dac29a2282cc3dcc446ed
Visin1991 <zhuzhanhao1991@gmail.com>
2020-04-25 11:48:48 +0800

    add numpy module

---
+++ Chapter03/03_PythonModulesPackages/build/CMakeFiles/pure-embedding.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "use_numpy.py"
+  "CMakeFiles/pure-embedding.dir/Py3-pure-embedding.cpp.o"
+  "pure-embedding.pdb"
+  "pure-embedding"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pure-embedding.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
